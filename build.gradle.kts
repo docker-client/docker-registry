0318a5d58dad04d46cb4be6ecdb384070fdd3555
Tobias Gesellchen <tobias@gesellix.de>
2022-02-27 13:12:52 +0100

    Add CI and publishing config

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
+rootProject.extra.set("artifactVersion", SimpleDateFormat("yyyy-MM-dd\'T\'HH-mm-ss").format(Date()))
+
+plugins {
+  id("maven-publish")
+  id("com.github.ben-manes.versions") version "0.42.0"
+  id("net.ossindex.audit") version "0.4.11"
+  id("io.freefair.maven-central.validate-poms") version "6.4.1"
+  id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+val dependencyVersions = listOf(
+  "org.jetbrains:annotations:23.0.0"
+)
+
+val dependencyVersionsByGroup = mapOf<String, String>(
+)
+
+subprojects {
+  repositories {
+//    mavenLocal()
+//    fun findProperty(s: String) = project.findProperty(s) as String?
+//    maven {
+//      name = "github"
+//      setUrl("https://maven.pkg.github.com/docker-client/*")
+//      credentials {
+//        username = System.getenv("PACKAGE_REGISTRY_USER") ?: findProperty("github.package-registry.username")
+//        password = System.getenv("PACKAGE_REGISTRY_TOKEN") ?: findProperty("github.package-registry.password")
+//      }
+//    }
+    mavenCentral()
+  }
+}
+
+allprojects {
+  configurations.all {
+    resolutionStrategy {
+      failOnVersionConflict()
+      force(dependencyVersions)
+      eachDependency {
+        val forcedVersion = dependencyVersionsByGroup[requested.group]
+        if (forcedVersion != null) {
+          useVersion(forcedVersion)
+        }
+      }
+    }
+  }
+}
+
+fun findProperty(s: String) = project.findProperty(s) as String?
+
+val isSnapshot = project.version == "unspecified"
+nexusPublishing {
+  repositories {
+    if (!isSnapshot) {
+      sonatype {
+        // 'sonatype' is pre-configured for Sonatype Nexus (OSSRH) which is used for The Central Repository
+        stagingProfileId.set(System.getenv("SONATYPE_STAGING_PROFILE_ID") ?: findProperty("sonatype.staging.profile.id")) //can reduce execution time by even 10 seconds
+        username.set(System.getenv("SONATYPE_USERNAME") ?: findProperty("sonatype.username"))
+        password.set(System.getenv("SONATYPE_PASSWORD") ?: findProperty("sonatype.password"))
+      }
+    }
+  }
+}
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,24 +1,129 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
 plugins {
-  groovy
-  `java-library`
+  id("groovy")
+  id("java-library")
+  id("maven-publish")
+  id("signing")
+  id("com.github.ben-manes.versions")
+  id("net.ossindex.audit")
+  id("io.freefair.maven-central.validate-poms")
 }
 
-repositories {
-  mavenCentral()
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
+  constraints {
+    listOf(
+      "org.jetbrains.kotlin:kotlin-reflect",
+      "org.jetbrains.kotlin:kotlin-stdlib",
+      "org.jetbrains.kotlin:kotlin-stdlib-jdk8",
+      "org.jetbrains.kotlin:kotlin-stdlib-common",
+      "org.jetbrains.kotlin:kotlin-test"
+    ).onEach {
+      implementation(it) {
+        version {
+          strictly("[1.5,1.7)")
+          prefer("1.6.10")
+        }
+      }
+    }
+    implementation("com.squareup.okio:okio") {
+      version {
+        strictly("[2.5,4)")
+        prefer("3.0.0")
+      }
+    }
+  }
   implementation("de.gesellix:docker-remote-api-client:2022-02-23T13-45-00")
   implementation("de.gesellix:docker-remote-api-model-1-41:2022-02-23T11-47-00")
   implementation("de.gesellix:docker-engine:2022-02-22T23-12-00")
 
-  implementation("org.slf4j:slf4j-api:[1.7,)")
+  implementation("org.slf4j:slf4j-api:[1.7,)!!1.7.36")
   testImplementation("ch.qos.logback:logback-classic:[1.2,2)!!1.2.10")
 
-  testImplementation("org.codehaus.groovy:groovy:3.0.9")
-  testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
+  testImplementation("org.codehaus.groovy:groovy:[3,4)!!3.0.9")
+  testImplementation("org.spockframework:spock-core:2.1-groovy-3.0")
 }
 
 tasks.withType<Test> {
   useJUnitPlatform()
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+  dependsOn("classes")
+  archiveClassifier.set("javadoc")
+  from(tasks.javadoc)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+  dependsOn("classes")
+  archiveClassifier.set("sources")
+  from(sourceSets.main.get().allSource)
+}
+
+artifacts {
+  add("archives", sourcesJar.get())
+  add("archives", javadocJar.get())
+}
+
+fun findProperty(s: String) = project.findProperty(s) as String?
+
+val isSnapshot = project.version == "unspecified"
+val artifactVersion = if (!isSnapshot) project.version as String else SimpleDateFormat("yyyy-MM-dd\'T\'HH-mm-ss").format(Date())!!
+val publicationName = "dockerRegistry"
+publishing {
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/${property("github.package-registry.owner")}/${property("github.package-registry.repository")}")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR") ?: findProperty("github.package-registry.username")
+        password = System.getenv("GITHUB_TOKEN") ?: findProperty("github.package-registry.password")
+      }
+    }
+  }
+  publications {
+    register(publicationName, MavenPublication::class) {
+      pom {
+        name.set("docker-registry")
+        description.set("Docker Registry/Distribution abstraction")
+        url.set("https://github.com/docker-client/docker-registry")
+        licenses {
+          license {
+            name.set("MIT")
+            url.set("https://opensource.org/licenses/MIT")
+          }
+        }
+        developers {
+          developer {
+            id.set("gesellix")
+            name.set("Tobias Gesellchen")
+            email.set("tobias@gesellix.de")
+          }
+        }
+        scm {
+          connection.set("scm:git:github.com/docker-client/docker-registry.git")
+          developerConnection.set("scm:git:ssh://github.com/docker-client/docker-registry.git")
+          url.set("https://github.com/docker-client/docker-registry")
+        }
+      }
+      artifactId = "docker-docker-registry"
+      version = artifactVersion
+      from(components["java"])
+      artifact(sourcesJar.get())
+      artifact(javadocJar.get())
+    }
+  }
+}
+
+signing {
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  useInMemoryPgpKeys(signingKey, signingPassword)
+  sign(publishing.publications[publicationName])
 }
