ae14cfb7c9f616dbe9b2afca931dd3cb5d0f1e3b
Tobias Gesellchen <tobias@gesellix.de>
2024-11-24 10:56:16 +0100

    Update to distribution v3 rc.1
    See https://github.com/distribution/distribution/releases/tag/v3.0.0-rc.1

--- lib/src/main/java/de/gesellix/docker/registry/DockerRegistry.java
+++ lib/src/main/java/de/gesellix/docker/registry/DockerRegistry.java
@@ -37,9 +37,9 @@ public class DockerRegistry {
     this.imageApi = new ImageApi(dockerClientConfig);
 
     if (LocalDocker.isNativeWindows()) {
-      imageNameWithTag = "gesellix/registry:3.0.0-alpha.1-windows-ltsc2022";
+      imageNameWithTag = "gesellix/registry:3.0.0-rc.1-windows-ltsc2022";
     } else {
-      imageNameWithTag = "registry:3.0.0-alpha.1";
+      imageNameWithTag = "registry:3.0.0-rc.1";
     }
     imageNameWithTag = System.getProperty("DOCKER_REGISTRY_IMAGE_OVERRIDE", imageNameWithTag);
   }
